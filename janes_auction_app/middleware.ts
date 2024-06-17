3b16c92795dc648e1a24d7a427d882cc864ba7ec
Sayo Adegoroye <steve.legion94@gmail.com>
2024-06-17 17:10:21 +0100

    Update middleware.ts

--- janes_auction_app/middleware.ts
+++ janes_auction_app/middleware.ts
@@ -1,10 +1,5 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
 export default clerkMiddleware();
-
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
