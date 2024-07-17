dfe58c355ced1f5e1844b911cd96b6abaa9f0226
Sayo Adegoroye <steve.legion94@gmail.com>
2024-07-17 02:45:01 +0100

    new page

--- janes_auction_app/app/(root)/profile/page.tsx
+++ janes_auction_app/app/(root)/profile/page.tsx
@@ -1,40 +1,7 @@
-// pages/profile/index.js
-"use client";
+import React from "react";
 
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+const Profile = () => {
+  return <div>Profile</div>;
+};
 
-export default function Profile() {
-  const searchParams = useSearchParams();
-  const section = searchParams.get("orders");
-
-  return (
-    <div>
-      <h1>Profile Page</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/profile/orders">
-              <h2>Orders</h2>
-            </Link>
-          </li>
-          <li>
-            <Link href="/profile/products">
-              <h2>Products</h2>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      {section === "orders" && <Orders />}
-      {section === "products" && <Products />}
-    </div>
-  );
-}
-
-function Orders() {
-  return <div>Orders Section</div>;
-}
-
-function Products() {
-  return <div>Products Section</div>;
-}
+export default Profile;
